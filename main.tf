216e218b8f63e9f1238b7afc4d363ef5f689de80
Victor Miguel <150725195+Victor-Miguel1405@users.noreply.github.com>
2025-11-16 18:26:23 +0000

    Terraform working

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 provider "azurerm" {
   features {}
+  subscription_id = "27f13d13-2f09-4e87-98b6-d36e2e7aac18"
+  tenant_id       = "6f9e3b1e-1809-444a-81d3-82d40a928812"
+  use_cli         = true
 }
 
 resource "azurerm_resource_group" "rg_devops" {
@@ -8,7 +11,7 @@ resource "azurerm_resource_group" "rg_devops" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "storagedevopsequipe"
+  name                     = "storagedevopsequipe3"
   resource_group_name      = azurerm_resource_group.rg_devops.name
   location                 = azurerm_resource_group.rg_devops.location
   account_tier             = "Standard"
